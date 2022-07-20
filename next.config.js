cdc8392edcf0ebc5211c74663e6b0382ade7aac0
Daniel <tokdaniel4@gmail.com>
2022-07-20 13:22:41 +0200

    gtags

--- next.config.js
+++ next.config.js
@@ -11,6 +11,7 @@ const nextConfig = {
     ALCHEMY_RPC_RINKEBY: process.env.ALCHEMY_RPC_RINKEBY,
     ALCHEMY_RPC_POLYGON_MAINNET: process.env.ALCHEMY_RPC_POLYGON_MAINNET,
     ALCHEMY_RPC_MUMBAI: process.env.ALCHEMY_RPC_MUMBAI,
+    NEXT_PUBLIC_GOOGLE_ANALYTICS: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS,
   },
   productionBrowserSourceMaps: true,
   optimizeFonts: false,
