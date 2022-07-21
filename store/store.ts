f423510633fd97d13d31876b30c1afdc019b2850
Daniel <tokdaniel4@gmail.com>
2022-07-21 17:55:47 +0200

    Revert "devtools settings"
    This reverts commit 8e621ab6fe46675acd9d71935d3c3960bc9fa714.

--- store/store.ts
+++ store/store.ts
@@ -15,26 +15,10 @@ export type State = ReturnType<typeof reducer>
 
 const epicMiddleware = createEpicMiddleware<AnyAction, AnyAction, State>()
 
-<<<<<<< Updated upstream
 export const store = configureStore({
   middleware: [epicMiddleware],
   reducer,
 })
-=======
-export const initStore = () => {
-  const store = configureStore({
-    middleware: [epicMiddleware],
-    reducer,
-    devTools: process.env.NODE_ENV === "development",
-  })
-
-  epicMiddleware.run(rootEpic)
-
-  return store
-}
-
-const store = initStore()
->>>>>>> Stashed changes
 
 epicMiddleware.run(rootEpic)
 
