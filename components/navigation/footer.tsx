365bd725da12d7486a104246a5e4c38235895f00
Daniel <tokdaniel4@gmail.com>
2022-07-26 12:34:44 +0200

    social links

--- components/navigation/footer.tsx
+++ components/navigation/footer.tsx
@@ -1,10 +1,11 @@
 import { Text } from "components/typography"
 import { useTranslation } from "next-i18next"
-import { FC } from "react"
+import { FC, ReactNode } from "react"
+import { FaGithub, FaLinkedin } from "react-icons/fa"
 import "twin.macro"
 import { FooterContainer } from "./navigation.styled"
 
-const FooterList: FC<{ heading: string; items: string[] }> = ({
+const FooterList: FC<{ heading: string; items: (string | ReactNode)[] }> = ({
   heading,
   items,
 }) => (
@@ -37,7 +38,28 @@ const Footer: FC = () => {
         />
         <FooterList
           heading={t("social.social")}
-          items={[t("social.github"), t("social.linkedin")]}
+          items={[
+            <a
+              href="https://github.com/nsorcell"
+              target="_blank"
+              tw="flex gap-1 items-center"
+            >
+              <FaGithub tw="text-white" />
+
+              {t("social.github")}
+            </a>,
+            <a
+              href="https://www.linkedin.com/in/daniel-tok-015ba6120/"
+              target="_blank"
+              tw="flex gap-1 items-center"
+            >
+              <div tw="bg-white box-shadow[inset 2px 2px black, inset -2px -2px black]">
+                <FaLinkedin tw="text-blue-500" />
+              </div>
+
+              {t("social.linkedin")}
+            </a>,
+          ]}
         />
         <FooterList
           heading={t("legal.legal")}
